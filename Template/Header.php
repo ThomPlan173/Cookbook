9fa34245405695b002127b86093aa488dec00323
007personne <129496105+007personne@users.noreply.github.com>
2023-03-31 15:20:53 +0200

    page recette

--- Template/Header.php
+++ Template/Header.php
@@ -3,7 +3,7 @@
         <div id="header2">
             <img id="index" src=Template/img/index.png onclick="window.location.href = 'index.php';">
             <div id="content_bouton_header">
-
+                <button class="button_header" onclick="window.location.href = 'recette.php';"><h1>Recette</h1></button>
             </div>
         <div>
     </div>
