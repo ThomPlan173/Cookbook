8bc5e94bea04136d9ecff44f3452e5e0ee91080d
007personne <129496105+007personne@users.noreply.github.com>
2023-03-31 14:24:36 +0200

    modif footer

--- Template/Footer.php
+++ Template/Footer.php
@@ -2,7 +2,7 @@
 
     <div class="footer">
         <span id="copyright"> <a>Copyright</a> © 2023 Constant Clément, Plantefèvre Thomas, Peron Alexandre. <br>
-
+            src
         </span>
     </div>
 
