d2d6d690a60689a1642afca896ecb9cd57579eae
007personne <129496105+007personne@users.noreply.github.com>
2023-03-31 11:12:44 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php require_once __DIR__.DIRECTORY_SEPARATOR."/Template/Template.php";?>
+
+<?php ob_start() ?>
+<?php $content = ob_get_clean() ?>
+
+<?php ob_start() ?>
+<?php $css = ob_get_clean() ?>
+
+<?php ob_start() ?>
+
+<script src="JS/index.js"></script>
+
+<?php $js = ob_get_clean() ?>
+
+<?php Template::render($content, $css, $js)?>
