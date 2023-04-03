303db1570e9bdc4bb09140ba40d3baa7863949c4
Alexandre <alexandreperon31@gmail.com>
2023-04-04 00:57:26 +0200

    Ajout des Classes/Pages

---
+++ class/Rec_Ig/Ingredient.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Rec_Ig;
+
+class Ingredient
+{
+
+}
--- class/Template/Header.php
+++ class/Template/Header.php
@@ -3,7 +3,7 @@
         <div id="header2">
             <img id="index" src=class/Template/img/index.png onclick="window.location.href = 'index.php';">
             <div id="content_bouton_header">
-                <button class="button_header" onclick="window.location.href = 'recette.php';"><h1>Recette</h1></button>
+                <button class="button_header" onclick="window.location.href = './pages/recette.php';"><h1>Recette</h1></button>
                 <button class="button_header" onclick="window.location.href = './class/cb/form_Recette.php';"><h1>Ajouter une recette</h1></button>
             </div>
         <div>
