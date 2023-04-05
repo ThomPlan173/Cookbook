a0a22c656fd1ca15021bf6e8dbf2c815e7917681
Thomas Plantefeve <84777732+ThomPlan173@users.noreply.github.com>
2023-04-05 08:30:23 +0200

    formulaire fonction

--- class/Template/Header.php
+++ class/Template/Header.php
@@ -4,7 +4,7 @@
             <img id="index" src=class/Template/img/index.png onclick="window.location.href = 'index.php';">
             <div id="content_bouton_header">
                 <button class="button_header" onclick="window.location.href = 'recette.php';"><h1>Recette</h1></button>
-                <button class="button_header" onclick="window.location.href = './class/cb/form_Recette.php';"><h1>Ajouter une recette</h1></button>
+                <button class="button_header" onclick="window.location.href = 'formular_recette.php';"><h1>Ajouter une recette</h1></button>
             </div>
         <div>
     </div>
