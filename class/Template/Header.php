cb8984a67b7d1857768a258b340071a023c3500a
Alexandre <alexandreperon31@gmail.com>
2023-04-06 17:24:42 +0200

    correction chemins

--- class/Template/Header.php
+++ class/Template/Header.php
@@ -3,9 +3,9 @@
         <div id="header2">
             <img id="index" src=/Projet_Recettes/class/Template/img/index.png onclick="window.location.href = '/Projet_Recettes/index.php';">
             <div id="content_bouton_header">
-                <button class="button_header" onclick="window.location.href = 'pages/recette.php';"><h1>Recette</h1></button>
+                <button class="button_header" onclick="window.location.href = '/Projet_Recettes/pages/recette.php';"><h2>Recette</h2></button>
                 <?php if(isset($_SESSION['login'])): ?>
-                <button class="button_header" onclick="window.location.href = 'formular_recette.php';"><h1>Ajouter une recette</h1></button>
+                <button class="button_header" onclick="window.location.href = '/Projet_Recettes/formular_recette.php';"><h2>Ajouter une recette</h2></button>
                 <?php endif;?>
             </div>
         <div>
