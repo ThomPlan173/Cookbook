9924f03789979f36506a59cf35cfdd6f4c3a1b29
007personne <129496105+007personne@users.noreply.github.com>
2023-04-07 13:34:22 +0200

    ne pas pull

--- index.php
+++ index.php
@@ -1,33 +1,33 @@
 <?php
- 
+session_start();
 
 require "." . DIRECTORY_SEPARATOR . 'class' . DIRECTORY_SEPARATOR . 'Autoloader.php';
 Autoloader::register();
-$_SESSION['page'] = "ingredients";
+$_SESSION['page'] = "ingredient";
 ?>
 
 <?php ob_start() ?>
+
+<fieldset id="type_search">
+    <legend>Type de recherche:</legend>
+
+    <div>
+        <input type="radio" id="type" name="type" value="nomRecette" checked>
+        <label for="nom">Par nom</label>
+    </div>
+
+    <div>
+        <input type="radio" id="type" name="type" value="nomIngrédient">
+        <label for="ingredient">Par ingrédient</label>
+    </div>
+
+    <div>
+        <input type="radio" id="type" name="type" value="nomTag">
+        <label for="louie">Par tag</label>
+    </div>
+</fieldset>
+
 <form action="./index.php" method="post">
-    <fieldset id="type_search">
-        <legend>Type de recherche:</legend>
-
-        <div>
-            <input type="radio" id="type" name="method" value="nomRecette" checked>
-            <label for="nom">Par nom</label>
-        </div>
-
-        <div>
-            <input type="radio" id="type" name="method" value="nomIngrédient">
-            <label for="ingredient">Par ingrédient</label>
-        </div>
-
-        <div>
-            <input type="radio" id="type" name="method" value="nomTag">
-            <label for="louie">Par tag</label>
-        </div>
-
-    </fieldset>
-
     <div class="search">
         <div>
             <h1>Recherche :</h1>
@@ -39,12 +39,12 @@ $_SESSION['page'] = "ingredients";
             <legend>Préférences de recherche:</legend>
 
             <div>
-                <input type="radio" id="" name="Preference" value="Alphabet" checked>
+                <input type="radio" id="huey" name="drone" value="huey" checked>
                 <label for="huey">Alphabétique ( A-Z )</label>
             </div>
 
             <div>
-                <input type="radio" id="dewey" name="Preference" value="Anti-Alphabet">
+                <input type="radio" id="dewey" name="drone" value="dewey">
                 <label for="dewey">Alphabétique inversée ( Z-A )</label>
             </div>
 
@@ -53,35 +53,37 @@ $_SESSION['page'] = "ingredients";
     </div>
 </form>
 
-<?php var_dump($_POST); ?>
+
+
+
 
 
 <div id="liste_recette">
-    <?php if (isset($_POST["nom"])) {
-        $cb = new \cb\CoobookDB();
-        $data = $cb->search($_POST["nom"], $_POST["method"]);
-        if (!empty($data)) {
-            foreach ($data as $d) {
-
-    ?>
-                <form method='get' action="pages/recette.php">
-                    <div class="recette">
-                        <button class="bouton_image_recette" type="submit" id='photo_tete' name="msg" value='<?= $d->idRecette ?>'>
-                            <img class="image_recette" src="<?= $d->imgRecette ?>">
-                        </button>
-                        <div>
-                            <h3><?= utf8_encode( $d->nomRecette) ?> :</h3>
-                            <?= utf8_encode($d->Description) ?>
-                        </div>
+<?php if (isset($_POST["nom"])) {
+    $cb = new \cb\CoobookDB();
+    $data = $cb->search($_POST["nom"],$_POST["method"]);
+    if (!empty($data)) {
+        foreach ($data as $d) {
+           
+            ?>
+            <form method='get' action="pages/recette.php" >
+                <div class="recette">
+                    <button class="bouton_image_recette" type="submit" id='photo_tete' name="msg" value='<?=$d->idRecette?>'>
+                        <img class="image_recette" src="<?=$d->imgRecette?>">
+                    </button>
+                    <div>
+                        <h3><?=$d->nomRecette?> :</h3>
+                        <?=$d->Description?>
                     </div>
-                </form>
+                </div>
+            </form>
 
-    <?php
-            }
-        } else {
-            echo "Aucune recette trouvée...";
+            <?php
         }
-    }; ?>
+    } else {
+        echo "Aucune recette trouvée...";
+    }
+}; ?>
 </div>
 <?php $content = ob_get_clean() ?>
 
@@ -98,3 +100,4 @@ $_SESSION['page'] = "ingredients";
 <?php $js = ob_get_clean() ?>
 
 <?php Template::render($content, $css, $js) ?>
+
