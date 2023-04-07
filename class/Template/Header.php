3337bc7c9c62732a230bb33296bc95d395e4b597
007personne <129496105+007personne@users.noreply.github.com>
2023-04-07 11:42:42 +0200

    header

--- class/Template/Header.php
+++ class/Template/Header.php
@@ -3,7 +3,6 @@
         <div id="header2">
             <img id="index" src=/Projet_Recettes/class/Template/img/index.png onclick="window.location.href = '/Projet_Recettes/index.php';">
             <div id="content_bouton_header">
-                <button class="button_header" onclick="window.location.href = '/Projet_Recettes/pages/recette.php';"><h2>Recette</h2></button>
                 <?php if(isset($_SESSION['login'])): ?>
                 <button class="button_header" onclick="window.location.href = '/Projet_Recettes/formular_recette.php';"><h2>Ajouter une recette</h2></button>
                 <?php endif;?>
