39b6d44fd78f8c482d259a2fe712ee7b4fd3c5d3
007personne <129496105+007personne@users.noreply.github.com>
2023-04-14 15:35:42 +0200

    index js

--- JS/index.js
+++ JS/index.js
@@ -4,8 +4,10 @@ document.addEventListener('DOMContentLoaded', function () {
     let input_tag = document.getElementById("search_tag");
 
     let checkbox_ingr = document.getElementById("checkbox_ingredient");
-    let checkbax_tag = document.getElementById("checkbox_tag");
+    let checkbox_tag = document.getElementById("checkbox_tag");
 
+    let ingredient_select = [];
+    let tag_select = [];
 
     function removeAllChild(parent) {
         while (parent.firstChild) {
@@ -14,15 +16,27 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
 
-    for(let ingredient in vardataIngr){
+    for(let ingredient of vardataIngr){
 
         let input = document.createElement('input')
         input.type = "checkbox";
-        input.name = vardataIngr[ingredient].nomIngredient;
+        input.name = ingredient.nomIngredient;
+        input.addEventListener('input', (event) => {
+            if (event.currentTarget.checked) {
+                ingredient_select.push(ingredient.idIngredient);
+            } else {
+                for(let i = 0; i < ingredient_select.length; i++) {
+                    if (ingredient_select[i] == ingredient.idIngredient) {
+                        let tempon = ingredient_select.splice(i, 1);
+                    }
+                }
+            }
+        })
 
         let label = document.createElement('label');
-        label.htmlFor = vardataIngr[ingredient].nomIngredient;
-        label.innerHTML = vardataIngr[ingredient].nomIngredient;
+        label.htmlFor = ingredient.nomIngredient;
+        label.innerHTML = ingredient.nomIngredient;
+
         let br = document.createElement('br')
 
         checkbox_ingr.appendChild(input);
@@ -30,15 +44,28 @@ document.addEventListener('DOMContentLoaded', function () {
         checkbox_ingr.appendChild(br);
     }
 
-    for(let tag in vardataTag){
+
+
+    for(let tag of vardataTag){
 
         let input = document.createElement('input')
         input.type = "checkbox";
-        input.name = vardataTag[tag].nomTag;
+        input.name = tag.nomTag;
+        input.addEventListener('input', (event) => {
+            if (event.currentTarget.checked) {
+                tag_select.push(tag.idTag);
+            } else {
+                for(let i = 0; i < tag_select.length; i++) {
+                    if (tag_select[i] == tag.idTag) {
+                        let tempon = tag_select.splice(i, 1);
+                    }
+                }
+            }
+        })
 
         let label = document.createElement('label');
-        label.htmlFor = vardataTag[tag].nomTag;
-        label.innerHTML = vardataTag[tag].nomTag;
+        label.htmlFor = tag.nomTag;
+        label.innerHTML = tag.nomTag;
         let br = document.createElement('br')
 
         checkbox_tag.appendChild(input);
@@ -52,39 +79,73 @@ document.addEventListener('DOMContentLoaded', function () {
 
         removeAllChild(checkbox_ingr);
 
-        for (let ingredient in vardataIngr) {
+        for (let ingredient of vardataIngr) {
 
-            if(vardataIngr[ingredient].nomIngredient.toUpperCase().includes(input_ingr.value.toUpperCase())) {
+            if ((ingredient.nomIngredient.toUpperCase().includes(input_ingr.value.toUpperCase())) || (ingredient_select.indexOf(ingredient.idIngredient) != -1)){
+
                 let input = document.createElement('input')
                 input.type = "checkbox";
-                input.name = vardataIngr[ingredient].nomIngredient;
+                input.name = ingredient.nomIngredient;
+                input.addEventListener('input', (event) => {
+                    if (event.currentTarget.checked) {
+                        ingredient_select.push(ingredient.idIngredient);
+                    } else {
+                        for(let i = 0; i < ingredient_select.length; i++) {
+                            if (ingredient_select[i] == ingredient.idIngredient) {
+                                let tempon = ingredient_select.splice(i, 1);
+                            }
+                        }
+                    }
+                })
+                if(ingredient_select.indexOf(ingredient.idIngredient) != -1) {
+                    input.checked = true;
+                }
 
                 let label = document.createElement('label');
-                label.htmlFor = vardataIngr[ingredient].nomIngredient;
-                label.innerHTML = vardataIngr[ingredient].nomIngredient;
+                label.htmlFor = ingredient.nomIngredient;
+                label.innerHTML = ingredient.nomIngredient;
+
                 let br = document.createElement('br')
 
                 checkbox_ingr.appendChild(input);
                 checkbox_ingr.appendChild(label);
                 checkbox_ingr.appendChild(br);
+
             }
         }
     })
+
+
 
     input_tag.addEventListener('input', function (){
 
         removeAllChild(checkbox_tag);
 
-        for (let tag in vardataTag) {
+        for (let tag of vardataTag) {
 
-            if(vardataTag[tag].nomTag.toUpperCase().includes(input_tag.value.toUpperCase())) {
+            if((tag.nomTag.toUpperCase().includes(input_tag.value.toUpperCase())) || (tag_select.indexOf(tag.idTag) != -1)){
                 let input = document.createElement('input')
                 input.type = "checkbox";
-                input.name = vardataTag[tag].nomTag;
+                input.name = tag.nomTag;
+                input.addEventListener('input', (event) => {
+                    if (event.currentTarget.checked) {
+                        tag_select.push(tag.idTag);
+                    } else {
+                        for(let i = 0; i < tag_select.length; i++) {
+                            if (tag_select[i] == tag.idTag) {
+                                let tempon = tag_select.splice(i, 1);
+                            }
+                        }
+                    }
+                })
+                if(tag_select.indexOf(tag.idTag) != -1){
+                    input.checked = true;
+                }
 
                 let label = document.createElement('label');
-                label.htmlFor = vardataTag[tag].nomTag;
-                label.innerHTML = vardataTag[tag].nomTag;
+                label.htmlFor = tag.nomTag;
+                label.innerHTML = tag.nomTag;
+
                 let br = document.createElement('br')
 
                 checkbox_tag.appendChild(input);
@@ -93,7 +154,5 @@ document.addEventListener('DOMContentLoaded', function () {
             }
         }
     })
-
-
 
 })
