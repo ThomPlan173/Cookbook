986b43f04d6d947b4cb19ef437868cd38415c539
Alexandre <alexandreperon31@gmail.com>
2023-05-05 17:32:58 +0200

    edit add js file

--- JS/edit_add.js
+++ JS/edit_add.js
@@ -140,9 +140,56 @@ document.addEventListener('DOMContentLoaded', function () {
 
                 let br = document.createElement('br')
 
+                let delete_form_ingredient = document.createElement('form');
+                delete_form_ingredient.method = "get";
+                delete_form_ingredient.action = "pages/editIngredient.php";
+
+                let bouton_delete_ingredient = document.createElement('button');
+                bouton_delete_ingredient.type = "submit";
+                bouton_delete_ingredient.id = "photo_tete";
+                bouton_delete_ingredient.name = "idIngredient";
+                bouton_delete_ingredient.value = rit_select[i].idIngredient;
+
+                let edit = document.createElement('img');
+                edit.src = "images/pencil-square.png";
+                edit.width = 15;
+                edit.height = 15;
+
+
+                let form_edit_ingredient = document.createElement('form');
+                form_edit_ingredient.method = "post";
+
+
+                let bouton_edit_ingredient = document.createElement('button');
+                bouton_edit_ingredient.id = "photo_tete";
+                bouton_edit_ingredient.name = "del";
+                bouton_edit_ingredient.value = rit_select[i].idIngredient;
+                bouton_edit_ingredient.addEventListener("click", function() {
+                    if (window.confirm("Voulais vous suprimer l'ingredient :\n" +
+                        rit_select[i].nomIngredient)) {
+                        form2.action = "pages/deleteIngredient.php";
+                        bouton2.type = "submit";
+                    }
+                })
+
+                let del = document.createElement('img');
+                del.src = "images/trash-fill.png";
+                del.width = 15;
+                del.height = 15;
+
+                admin.appendChild(form1);
+                form1.appendChild(bouton1);
+                bouton1.appendChild(edit);
+
+                admin.appendChild(form2);
+                form2.appendChild(bouton2);
+                bouton2.appendChild(del);
+
                 checkbox_tag.appendChild(input);
                 checkbox_tag.appendChild(label);
+                checkbox_tag.appendChild(_form_ingredient);
                 checkbox_tag.appendChild(br);
+
             }
         }
         recherche();
