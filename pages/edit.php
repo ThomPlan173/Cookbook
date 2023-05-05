1387e3348346170a1bfcc4294b3a9e896a1e04c3
Thomas Plantefeve <84777732+ThomPlan173@users.noreply.github.com>
2023-05-05 16:23:09 +0200

    tags

--- pages/edit.php
+++ pages/edit.php
@@ -13,24 +13,24 @@ $ed = new \Edit\Edit();
 
 
 if (isset($_POST['submit'])) {
-    
-   
+
+
     $nom = $_POST['nom'];
     $img = "images/" . $_POST['image'];
     $description = $_POST['description'];
     $preparation = $_POST['preparation'];
-    $response = $ed->verif($nom,$img,$description,$preparation);
-    if ($response['granted']){
-        $result = $cb->updateRecette($id,$img,htmlspecialchars($nom,ENT_QUOTES),htmlspecialchars($description, ENT_QUOTES),htmlspecialchars($preparation, ENT_QUOTES));
-        header("Location: "."/Projet_Recettes/index.php");
-        exit() ;
+    $response = $ed->verif($nom, $img, $description, $preparation);
+    if ($response['granted']) {
+        $result = $cb->updateRecette($id, $img, htmlspecialchars($nom, ENT_QUOTES), htmlspecialchars($description, ENT_QUOTES), htmlspecialchars($preparation, ENT_QUOTES));
+        header("Location: " . "/Projet_Recettes/index.php");
+        exit();
     }
 }
 ?>
 
-<?php $dataRit = $cb->getAllRIT() ; ?>
+<?php $dataRit = $cb->getAllRIT(); ?>
 
-<?php ob_start() ;
+<?php ob_start();
 
 $sr->generateliste($cb); ?>
 
@@ -59,11 +59,11 @@ $sr->generateliste($cb); ?>
 <script src="../JS/index.js"></script>
 
 <script>
-    function recherche(){
+    function recherche() {
 
         removeAllChild(checkbox_rit);
         let n_ingredient = 0;
-        for(let id of ingredient_select) {
+        for (let id of ingredient_select) {
             if (rit.liste_de_ingredients != null) {
                 if (rit.liste_de_ingredients.indexOf(id) != -1) {
 
@@ -71,10 +71,10 @@ $sr->generateliste($cb); ?>
                 }
             }
         }
-        if(n_ingredient == ingredient_select.length){
+        if (n_ingredient == ingredient_select.length) {
             let n_tag = 0;
-            for(let id of tag_select) {
-                if(rit.liste_de_tags != null) {
+            for (let id of tag_select) {
+                if (rit.liste_de_tags != null) {
                     if (rit.liste_de_tags.indexOf(id) != -1) {
                         n_tag++;
                     }
@@ -84,16 +84,15 @@ $sr->generateliste($cb); ?>
         }
 
     }
-
 </script>
 
-<div id = "reste_page" >
+<div id="reste_page">
     <?php
 
     if (!isset($response)) :
-        $ed->generateformRecette($data[0]->nomRecette,$data[0]->imgRecette, $data[0]->Description, $data[0]->Preparation);
+        $ed->generateformRecette($data[0]->nomRecette, $data[0]->imgRecette, $data[0]->Description, $data[0]->Preparation);
     elseif (!$response['granted']) :
-        echo "<div class='error'>" ."Empty !"."</div>" ;
+        echo "<div class='error'>" . "Empty !" . "</div>";
         $ed->generateformRecette($response['error']);
     endif; ?>
 </div>
@@ -110,14 +109,13 @@ $sr->generateliste($cb); ?>
 
 <script></script>
 <?php var_dump($tags); ?>
-<?php var_dump($liste); ?>
 <?php $js = ob_get_clean() ?>
 
 <?php Template::render($content, $css, $js) ?>
 
 <?php foreach ($tags as $t) {
-    var_dump($t);
-} ?>
-<script>
-    
-</script>
+    echo $t->nomTag; ?>
+    <script>
+        document.getElementsByName("<?= $t->nomTag; ?>").checked = true;
+    </script>
+<?php  } ?>
